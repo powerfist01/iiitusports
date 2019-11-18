da0e591a83327e54cb5db9f17fc9bef27d5f2542
powerfist01 <sujeets207@gmail.com>
2019-11-19 04:13:08 +0530

    Added event page

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,10 @@ Router.get('/',function(req,res){
     res.render('index');
 })
 
+Router.get('/events',function(req,res){
+    res.render('events');
+})
+
 Router.get('/notfound',function(req,res){
     res.render('notfound');
 })
