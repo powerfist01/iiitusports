acdc1c2c708a8154431920744af780e132133bf4
powerfist01 <sujeets207@gmail.com>
2019-12-19 19:32:42 +0530

    Updated footballPlayer.ejs

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 const app = express();
 
 // setting the views folder and public folder
